24b953eb90a268bc600f784f9c2132d72f2ef152
Isai Vélez <ivelez@lwolf.com>
2025-04-24 14:02:08 -0500

    Agregar PeridoFactory

---
+++ PedidosApp/Factories/EntregaFactory.cs
@@ -0,0 +1,20 @@
+﻿using PedidosApp.Clases;
+using PedidosApp.Interfaces;
+
+namespace PedidosApp.Factories
+{
+    internal class EntregaFactory
+    {
+        public static IMetodoEntrega CrearEntrega(string tipoProducto, bool urgente, double peso)
+        {
+            if (tipoProducto == "tecnología" && urgente)
+                return new EntregaDron();
+            else if (tipoProducto == "accesorio")
+                return new EntregaMoto();
+            else if (tipoProducto == "componente" || peso > 10)
+                return new EntregaCamion();
+            else
+                return new EntregaMoto();
+        }
+    }
+}
